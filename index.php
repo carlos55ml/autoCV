22eda0769bc7f5a642ea5a9ddc566242263cdf21
carlos55ml <cmoranlozano55@gmail.com>
2023-01-27 13:14:00 +0100

    feat(index): button and format

--- index.php
+++ index.php
@@ -15,7 +15,9 @@ include_once __DIR__ . '/modules/head.php';
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container px-lg-5" data-bs-theme="dark">
       <a class="navbar-brand" href="/">AutoCV</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+          class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Inicio</a></li>
@@ -23,21 +25,21 @@ include_once __DIR__ . '/modules/head.php';
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               <?php echo $userObj['username'] ?? "Invitado" ?>
             </a>
-            <?php if($userObj) { ?>
+            <?php if ($userObj) { ?>
               <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Ver perfil</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="/controller/userHandler.php?action=logout">Cerrar sesion</a></li>
-            </ul>
-          </li>
+                <li><a class="dropdown-item" href="#">Ver perfil</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="/controller/userHandler.php?action=logout">Cerrar sesion</a></li>
+              </ul>
+            </li>
           <?php } else { ?>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="/login.php">Iniciar Sesion</a></li>
               <li><a class="dropdown-item" href="/register.php">Registrate</a></li>
             </ul>
-            <?php } ?>
+          <?php } ?>
 
         </ul>
       </div>
@@ -50,6 +52,14 @@ include_once __DIR__ . '/modules/head.php';
         <div class="m-4 m-lg-5">
           <h1 class="display-5 fw-bold">AutoCV</h1>
           <p class="fs-4">Crea tu Curriculum Vitae automaticamente.</p>
+
+        </div>
+        <div class="d-grid gap-2">
+          <?php if ($userObj) { ?>
+          <button class="btn btn-primary btn-lg" type="button"><a href="#" style="text-decoration: none; color:white;">Empezar</a></button>
+          <?php } else { ?>
+            <button class="btn btn-primary btn-lg" type="button"><a href="/login.php" style="text-decoration: none; color:white;">Empezar</a></button>
+          <?php } ?>
         </div>
       </div>
     </div>
@@ -62,7 +72,8 @@ include_once __DIR__ . '/modules/head.php';
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i
+                  class="bi bi-collection"></i></div>
               <h2 class="fs-4 fw-bold">Diseño dinamico</h2>
               <p class="mb-0">Elige los elementos que quieres mostrar y vea una vista previa del resultado</p>
             </div>
@@ -71,7 +82,8 @@ include_once __DIR__ . '/modules/head.php';
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-cloud-download"></i></div>
+              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i
+                  class="bi bi-cloud-download"></i></div>
               <h2 class="fs-4 fw-bold">Gratuito</h2>
               <p class="mb-0">Nuestras funciones estan disponibles para todos los usuarios.</p>
             </div>
@@ -80,7 +92,8 @@ include_once __DIR__ . '/modules/head.php';
         <div class="col-lg-6 col-xxl-4 mb-5">
           <div class="card bg-light border-0 h-100">
             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
-              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-card-heading"></i></div>
+              <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i
+                  class="bi bi-card-heading"></i></div>
               <h2 class="fs-4 fw-bold">Todas las funcionalidades</h2>
               <p class="mb-0">Separa el contenido por categorias y elementos, para ordenarlo a tu gusto.</p>
             </div>
@@ -109,8 +122,10 @@ include_once __DIR__ . '/modules/head.php';
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-          <button type="button" class="btn btn-primary"><a class="no-decors" href="/login.php">Inicia sesion</a></button>
-          <button type="button" class="btn btn-info"><a class="no-decors color-black" href="/register.php">Registrate</a></button>
+          <button type="button" class="btn btn-primary"><a class="no-decors" href="/login.php">Inicia
+              sesion</a></button>
+          <button type="button" class="btn btn-info"><a class="no-decors color-black"
+              href="/register.php">Registrate</a></button>
         </div>
       </div>
     </div>
